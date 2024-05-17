b3789654d5a37b1b183ab60f0a4d23fc0f3642c4
Vishal Pattar <104265753+Vishal-Pattar@users.noreply.github.com>
2024-05-17 21:48:33 +0530

    Update MusicPlayer.py

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -4,47 +4,57 @@ import flet as ft
 
 class MusicPlayer:
     def __init__(self):
-        self.music_directory = None
-        self.photo_directory = None
+        """Initialize the music player with default settings."""
+        self.music_directory = "Songs"
+        self.photo_directory = "Images"
         self.playlist = []
         self.photolist = []
         self.current_song = 0
         pygame.mixer.init()
-        
+        self.load_music()
+
     def load_music(self):
-        self.music_directory = "Songs"
-        self.photo_directory = "Images"
+        """Load music and images from the respective directories."""
         if self.music_directory:
-            self.playlist = []
-            for file in os.listdir(self.music_directory):
-                if file.endswith(".mp3"):
-                    self.playlist.append(os.path.join(self.music_directory, file))
+            self.playlist = [
+                os.path.join(self.music_directory, file)
+                for file in os.listdir(self.music_directory)
+                if file.endswith(".mp3")
+            ]
+        
         if self.photo_directory:
-            self.photolist = []
-            for pic in os.listdir(self.photo_directory):
-                if pic.endswith(".png"):
-                    self.photolist.append(os.path.join(self.photo_directory, pic))
+            self.photolist = [
+                os.path.join(self.photo_directory, pic)
+                for pic in os.listdir(self.photo_directory)
+                if pic.endswith(".png")
+            ]
 
     def play(self):
+        """Play the current song."""
         if self.playlist:
             pygame.mixer.music.load(self.playlist[self.current_song])
             pygame.mixer.music.play()
 
     def pause(self):
+        """Pause the current song."""
         pygame.mixer.music.pause()
 
     def stop(self):
+        """Stop the current song."""
         pygame.mixer.music.stop()
 
     def next_song(self):
+        """Play the next song in the playlist."""
         self.current_song = (self.current_song + 1) % len(self.playlist)
         self.play()
 
     def previous_song(self):
+        """Play the previous song in the playlist."""
         self.current_song = (self.current_song - 1) % len(self.playlist)
         self.play()
 
 def main(page: ft.Page):
+    """Set up the GUI for the music player application."""
     page.title = "Music App"
     page.window_height = 800
     page.window_width = 600
@@ -53,17 +63,24 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     music_player = MusicPlayer()
-    music_player.load_music()
 
-    head = ft.Text("Music App", size=25, text_align=ft.TextAlign.CENTER, weight=ft.FontWeight.W_900)
+    # Header text
+    head = ft.Text(
+        "Music App",
+        size=25,
+        text_align=ft.TextAlign.CENTER,
+        weight=ft.FontWeight.W_900
+    )
+
+    # Image display
     img = ft.Image(
         width=400,
         height=200,
-        fit=ft.ImageFit.CONTAIN
+        fit=ft.ImageFit.CONTAIN,
+        src=music_player.photolist[music_player.current_song]
     )
-    
-    img.src = music_player.photolist[music_player.current_song]
 
+    # Button click handlers
     def prev_sg(e):
         music_player.previous_song()
         img.src = music_player.photolist[music_player.current_song]
@@ -83,41 +100,66 @@ def main(page: ft.Page):
         img.src = music_player.photolist[music_player.current_song]
         page.update()
 
+    # Control buttons
+    iconrow = ft.Row(
+        [
+            ft.IconButton(
+                icon=ft.icons.SKIP_PREVIOUS_ROUNDED,
+                icon_color="pink400",
+                icon_size=40,
+                tooltip="Previous",
+                on_click=prev_sg
+            ),
+            ft.IconButton(
+                icon=ft.icons.PAUSE_CIRCLE_ROUNDED,
+                icon_color="pink400",
+                icon_size=50,
+                tooltip="Pause",
+                on_click=pause_sg
+            ),
+            ft.IconButton(
+                icon=ft.icons.PLAY_CIRCLE_ROUNDED,
+                icon_color="pink400",
+                icon_size=60,
+                tooltip="Play",
+                on_click=play_sg
+            ),
+            ft.IconButton(
+                icon=ft.icons.STOP_CIRCLE_ROUNDED,
+                icon_color="pink400",
+                icon_size=50,
+                tooltip="Stop",
+                on_click=stop_sg
+            ),
+            ft.IconButton(
+                icon=ft.icons.SKIP_NEXT_ROUNDED,
+                icon_color="pink400",
+                icon_size=40,
+                tooltip="Next",
+                on_click=next_sg
+            ),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
 
-    iconrow = ft.Row([
-        ft.IconButton(icon=ft.icons.SKIP_PREVIOUS_ROUNDED, icon_color="pink400", icon_size=40, tooltip="Previous", on_click=prev_sg),
-        ft.IconButton(icon=ft.icons.PAUSE_CIRCLE_ROUNDED, icon_color="pink400", icon_size=50, tooltip="Pause", on_click=pause_sg),
-        ft.IconButton(icon=ft.icons.PLAY_CIRCLE_ROUNDED, icon_color="pink400", icon_size=60, tooltip="Play", on_click=play_sg),
-        ft.IconButton(icon=ft.icons.STOP_CIRCLE_ROUNDED, icon_color="pink400", icon_size=50, tooltip="Stop", on_click=stop_sg),
-        ft.IconButton(icon=ft.icons.SKIP_NEXT_ROUNDED, icon_color="pink400", icon_size=40, tooltip="Next", on_click=next_sg),
-    ],alignment=ft.MainAxisAlignment.CENTER)
-
-    col = ft.Column([
-        ft.Container(
-            content=head,
-            alignment=ft.alignment.bottom_center,
-        ),
-        ft.Container(
-            content=img,
-            alignment=ft.alignment.bottom_right,
-        ),
-        ft.Container(
-            content=iconrow,
-            padding=5,
-            alignment=ft.alignment.center,
-        )
-    ],
-    width=400)
+    # Layout
+    col = ft.Column(
+        [
+            ft.Container(content=head, alignment=ft.alignment.bottom_center),
+            ft.Container(content=img, alignment=ft.alignment.bottom_right),
+            ft.Container(content=iconrow, padding=5, alignment=ft.alignment.center),
+        ],
+        width=400
+    )
 
     cont = ft.Container(
         content=col,
         padding=20,
         border_radius=10,
-        border=ft.border.all(2, '#000000'),
+        border=ft.border.all(2, '#000000')
     )
 
-    page.add(
-        cont,
-    )
+    page.add(cont)
 
+# Start the application
 ft.app(target=main)
